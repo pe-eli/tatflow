2b41b54a48687e041153481fada4ba311e04d8c7
unknown <p.mouuraa05@gmail.com>
2026-03-09 10:15:03 -0300

    Mais alterações na tela de Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -11,7 +11,7 @@ const Login: React.FC = () => {
   const [remember, setRemember] = useState(true)
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
-
+//
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     setError('')
